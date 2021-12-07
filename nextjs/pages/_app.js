32040ac48e97a7295e29cea77ca23799167e1e45
johnymontana <lyonwj@gmail.com>
2021-12-07 07:25:31 -0700

    add basic graphql data fetching

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function createApolloClient() {
+  const link = new HttpLink({
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
+  });
+
+  return new ApolloClient({
+    link,
+    cache: new InMemoryCache(),
+  });
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+  return (
+    <ApolloProvider client={createApolloClient()}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+
+export default MyApp;
---
+++ nextjs/pages/articles.js
@@ -0,0 +1,33 @@
+import { useQuery, gql } from "@apollo/client";
+
+const ArticleQuery = gql`
+  {
+    articles(options: { limit: 25, sort: { published: DESC } }) {
+      title
+      url
+      published
+    }
+  }
+`;
+
+const Articles = () => {
+  const { data } = useQuery(ArticleQuery);
+
+  return (
+    <div>
+      <ul>
+        {data?.articles.map((v) => {
+          return (
+            <li key={v.url}>
+              <a href={v.url} target="_blank">
+                {v.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Articles;
